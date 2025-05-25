518e26c40d85be46a4993a903d08f1820e1a0bba
N9 <nguyengiapham1@gmail.com>
2025-05-26 06:06:12 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,27 +1,22 @@
 Config = {}
 
-Config.Webhook = 'YOUR_DISCORD_WEBHOOK_URL' -- Webhook report
-
-Config.StaffWhitelist = {
-    'steam:11000010xxxxxx',
-    'steam:11000020yyyyyy'
+Config.BlacklistedWeapons = {
+    "WEAPON_RPG",
+    "WEAPON_STINGER",
+    "WEAPON_MINIGUN"
 }
 
 Config.BlacklistedVehicles = {
-    'RHINO',
-    'HYDRA',
-    'OPPRESSOR2',
+    "rhino",
+    "buzzard2"
 }
 
-Config.BlacklistedWeapons = {
-    'WEAPON_RPG',
-    'WEAPON_MINIGUN',
+Config.MaxEntitySpawns = 10
+Config.Webhook = "https://discord.com/api/webhooks/yourwebhook"
+Config.StaffWhitelist = {
+    "steam:110000112345678",
+    "steam:110000112345679"
 }
 
-Config.ValidateInventory = true          -- Check invalid items
-Config.MaxEntitySpawns = 10              -- Max entities in short time
-Config.UsePersistentBan = true           -- Persistent ban system
-Config.Locale = 'en'                     -- Language
-Config.EnableAdminPanel = true           -- Admin UI enable
-Config.ExportBanLogs = true              -- Export ban logs json
-Config.CloudBanSync = false              -- Cloud sync enable
+Config.Locale = 'en'
+Config.EnableAdminPanel = true
